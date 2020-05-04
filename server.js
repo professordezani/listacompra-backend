ca4dffbf7e4f84ad9c2b34342a9a6e6e8d83ec65
Prof. Dr. Henrique Dezani <dezani@fatecriopreto.edu.br>
2020-05-04 01:33:50 +0000

    Versão com o CRUD em memória.

--- server.js
+++ server.js
@@ -2,8 +2,55 @@ const express = require('express');
 
 const server = express();
 
-server.get('/', function(request, response) {
-    response.send('Hello World!');
+server.use(express.json());
+
+const produtos = [
+    { nome: 'Arroz', quantidade: 2},
+    { nome: 'Feijão', quantidade: 1} 
+]
+
+server.get('/produto', function(request, response) {
+    response.json(produtos);
+})
+
+server.post('/produto', function(request, response) {
+
+    // const nome = request.body.nome;
+    // const quantidade = request.body.quantidade;
+
+    const {nome, quantidade} = request.body;
+
+    produtos.push({nome, quantidade});
+    response.status(204).send();
+})
+
+server.put('/produto/:id', function(request, response) { 
+    const id = request.params.id;
+    const {nome, quantidade} = request.body;
+
+    for(let i = 0; i < produtos.length; i++) {
+        if(produtos[i].nome == id) {
+            produtos[i].nome = nome;
+            produtos[i].quantidade = quantidade;
+            break;
+        }
+    }
+
+    return response.status(204).send();
+})
+
+server.delete('/produto/:id', function(request, response) {
+
+    const id = request.params.id;
+
+    for(let i = 0; i < produtos.length; i++) {
+        if(produtos[i].nome == id) {
+            produtos.splice(i, 1);
+            break;
+        }
+    }
+
+    return response.status(204).send();
 })
 
 server.listen(process.env.PORT || 3000);
